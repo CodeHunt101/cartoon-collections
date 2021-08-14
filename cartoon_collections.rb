25fe6f2f547f690fa6ea0635d13e1fca7eeb397e
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-14 15:43:50 +1000

    Lab completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs_arr)
+  dwarfs_arr.each_with_index do |dwarf,index|
+    puts("#{index + 1}. #{dwarf}")
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls_arr)
+  final_arr = planteer_calls_arr.map do |call|
+    "#{call.capitalize}!"
+  end
+  final_arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any?{|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find do |item| 
+    cheese_types.any? {|cheese| item == cheese}
+  end
 end
